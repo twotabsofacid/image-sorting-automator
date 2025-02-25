f0c2899d3cba8ecbf5630f7300a44717184be1f6
picpicpicpicnic <sscanlan@deeplocal.com>
2025-02-25 08:08:46 -0500

    Updating with some horrible breaking changes for now. need to move image manipulation into backend to make this work

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 // Imports
 import 'dotenv/config';
 import { app, BrowserWindow, desktopCapturer } from 'electron';
+import axios from 'axios';
+import { google } from 'googleapis';
+import { generate, count } from 'random-words';
 import express from 'express';
 import path from 'node:path';
 import fs from 'node:fs';
 import http from 'node:http';
 import cors from 'cors';
+import { Server } from 'socket.io';
 // Variables
 const __dirname = path.resolve();
+const API_KEY = process.env.API_KEY;
 
 class Main {
   constructor() {
@@ -16,7 +21,9 @@ class Main {
     this.backend.use(express.json());
     this.backend.use(express.static(path.join(__dirname, 'public')));
     this.server = http.createServer(this.backend);
+    this.io = new Server(this.server);
     this.addRoutes();
+    this.addIO();
     this.start();
   }
   addRoutes() {
@@ -31,6 +38,15 @@ class Main {
       socket.on('connected', async () => {
         console.log('we connected');
       });
+      socket.on('video', (data) => {
+        console.log('WE ARE PLAYING A NEW VIDEO');
+        setTimeout(() => {
+          this.grabScreenshot();
+        }, 300000);
+      });
+      socket.on('image', (data) => {
+        console.log('we got an image', data, data.length);
+      });
     });
     this.io.attach(this.server, {
       cors: {
@@ -38,37 +54,103 @@ class Main {
       }
     });
   }
+  grabScreenshot() {
+    console.log('Taking screenshot...');
+    desktopCapturer
+      .getSources({
+        types: ['window'],
+        thumbnailSize: {
+          height: 768,
+          width: 1366
+        }
+      })
+      .then((sources) => {
+        for (let s in sources) {
+          if (sources[s].name === 'Screengrabber') {
+            const content = sources[s].thumbnail.toPNG();
+            console.log(content);
+            fs.writeFile(`out/screen-${Date.now()}.png`, content, (error) => {
+              if (error) {
+                console.error('Error saving PNG file:', error);
+              } else {
+                console.log('PNG file saved successfully!');
+              }
+            });
+          }
+        }
+      });
+  }
+  async getAccessToken() {
+    const auth = new google.auth.GoogleAuth({
+      keyFilename: 'service-account-key.json',
+      // Scopes can be specified at the constructor or at the time of usage.
+      scopes: ['https://www.googleapis.com/auth/youtube.readonly']
+    });
+
+    const client = await auth.getClient();
+    const accessToken = (await client.getAccessToken()).token;
+
+    return accessToken;
+  }
+  async getRandomVideoID() {
+    console.log('getting random video id');
+    return new Promise((resolve, reject) => {
+      this.getAccessToken()
+        .catch((e) => {
+          reject(e);
+        })
+        .then(async (token) => {
+          try {
+            const randomWords = generate(2).join(' ');
+            console.log('random words', randomWords);
+            const response = await axios({
+              method: 'get',
+              url: 'https://youtube.googleapis.com/youtube/v3/search',
+              params: {
+                part: 'snippet',
+                q: randomWords,
+                maxResults: 4,
+                order: 'viewCount',
+                type: 'video',
+                key: API_KEY // API Key
+              },
+              headers: {
+                Authorization: `Bearer ${token}`, // Replace with your actual access token
+                Accept: 'application/json'
+              }
+            });
+            if (
+              response.data &&
+              response.data.items &&
+              response.data.items.length
+            ) {
+              console.log('we got data', response.data.items.length);
+              resolve(response.data.items[response.data.items.length - 1]);
+            } else {
+              reject('No data received');
+            }
+          } catch (error) {
+            console.error(
+              'Error:',
+              error.response ? error.response.data : error.message
+            );
+            reject(error);
+          }
+        });
+    });
+  }
   start() {
     this.server.listen(6969, () => {
       console.log(`Server listening on port ${6969}`);
     });
-    setTimeout(() => {
-      console.log('SCREENSHOT');
-      desktopCapturer
-        .getSources({
-          types: ['window'],
-          thumbnailSize: {
-            height: 768,
-            width: 1366
-          }
-        })
-        .then((sources) => {
-          for (let s in sources) {
-            console.log('s and sources', s, sources);
-            if (sources[s].name === 'Screengrabber') {
-              const content = sources[s].thumbnail.toPNG();
-              console.log(content);
-              fs.writeFile('test.png', content, (error) => {
-                if (error) {
-                  console.error('Error saving PNG file:', error);
-                } else {
-                  console.log('PNG file saved successfully!');
-                }
-              });
-            }
-          }
-        });
-    }, 5000);
+    // setInterval(() => {
+    //   this.getRandomVideoID()
+    //     .catch(console.error)
+    //     .then((video) => {
+    //       console.log('what the fuck we have something', video.snippet.title);
+    //       this.io.emit('video', video);
+    //     });
+    // }, 10000);
   }
 }
 
